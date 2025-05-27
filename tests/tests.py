b39c857cf13fd57210aa107567c3bd0ba8bdb032
PedroCv <gonryuk0001@gmail.com>
2025-05-27 19:22:05 +0100

    unittests

--- tests/tests.py
+++ tests/tests.py
@@ -1,24 +1,22 @@
 import unittest
 import sqlite3
-from database.database import create_db, check_url, update_hits
+from database.database import create_db,insert_url, check_url, update_hits, get_all_urls
+import random
+import string
 from short_url import app
 
 class short_url_test(unittest.TestCase):
-        def setUpClass(cls):
-            cls.db_name="test_database.db"
-            conection=sqlite3.connect(cls.db_name)
-            cursor=conection.cursor()
-            cursor.execute(
-                '''
-                CREATE TABEL IF NOT EXISTS urls (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                long_url TEXT NOT NULL,
-                short_url TEXT NOT NULL UNIQUE,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                hits INTEGER DEFAULT 0,
-                )
-                '''
-            )
-            conection.commit()
-            conection.close()
-            
+
+
+    def test_check_url(self):
+        short="".join(random.choices(string.ascii_letters + string.digits, k=6))
+        print(short)
+        insert_url("https://www.google.com/search?q=python+is+life", short)
+        long_url= check_url(short)
+        self.assertEqual(long_url, "https://www.google.com/search?q=python+is+life")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
